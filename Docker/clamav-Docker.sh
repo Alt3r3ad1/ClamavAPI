8aead53e2508afa8f37fb52779ed44bb43e953c6
Alt3r3ad1 <55192547+Alt3r3ad1@users.noreply.github.com>
2023-10-22 07:04:45 -0800

    Update clamav-Docker.sh

--- Docker/clamav-Docker.sh
+++ Docker/clamav-Docker.sh
@@ -2,7 +2,7 @@
 imageName="clamav"
 
 while true; do
- if [ ! $(docker container inspect $imageName 2> /dev/null | grep "Running" | cut -d ":" -f2 | sed "s/ //" | sed "s/,//") ]; then
+ if [ $(docker container inspect $imageName 2> /dev/null | grep "Running" | cut -d ":" -f2 | sed "s/ //" | sed "s/,//") ]; then
   if [ "$(docker inspect -f '{{.State.Health.Status}}' $imageName)" != "healthy" ] && [ "$(docker inspect -f '{{.State.Health.Status}}' $imageName)" != "starting" ]; then
    docker rm -f $imageName >/dev/null 2>&1
 
